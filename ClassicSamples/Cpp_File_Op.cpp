118bb3a648146fdb1ef3b8d226753bf287daa0a6
FinixLei <finixlei@gmail.com>
2015-10-05 20:40:52 +0800

    add ClassicSamples/Cpp_File_Op.cpp

---
+++ ClassicSamples/Cpp_File_Op.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char ** argv) {
+    const int size = 4096;
+    char line[size]; 
+    
+    char path[] = "ClassicSamples/Cpp_File_Op.cpp";
+    
+    ifstream fin(path, ios::in);
+    
+    if (!fin.good()) {
+        cout << "Error happened when trying to open: " << path << endl; 
+        return -1;
+    }
+    
+    while(!fin.eof()) {
+        memset(line, 0, size);
+        fin.getline(line, size);
+        cout << line <<endl; 
+    }
+    
+    fin.clear();  // 清除文件流的指针位置
+    fin.seekg(0, ios::beg);  // 重新定位到文件开始位置
+    
+    // 打印第二遍
+    while(!fin.eof()) {
+        memset(line, 0, size);
+        fin.getline(line, size);
+        cout << line <<endl; 
+    }
+    
+    fin.close(); 
+    
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: string_in_c cpp_base_samples c_base_samples \
-     singleton C_File_Op
+     singleton C_File_Op Cpp_File_Op
      
 clean:
 	rm string_in_c cpp_base_samples c_base_samples \
-    singleton C_File_Op
+    singleton C_File_Op Cpp_File_Op
     
 string_in_c: string_in_c.c
 	gcc -o string_in_c string_in_c.c
@@ -19,3 +19,6 @@ singleton: ClassicSamples/Singleton.cpp
     
 C_File_Op: ClassicSamples/C_File_Op.c
 	g++ -o C_File_Op ClassicSamples/C_File_Op.c
+
+Cpp_File_Op: ClassicSamples/Cpp_File_Op.cpp
+	g++ -o Cpp_File_Op ClassicSamples/Cpp_File_Op.cpp
