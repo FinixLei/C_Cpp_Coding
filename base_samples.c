d61b27ff2113dd7167c0f80fa4eb4b1a51e8856f
FinixLei <finixlei@gmail.com>
2015-10-05 16:25:43 +0800

    add test_check_assigned_unassigned()

--- base_samples.c
+++ base_samples.c
@@ -1,8 +1,13 @@
-#define CONVERT(T) \
+#include <stdio.h>
+
+/* 
+    write a macro to convert the big endian/little endian to little endian/big endian. 
+*/
+void test_convert_bigendian_littleendian() {
+    #define CONVERT(T) \
 (*T) ^= (*(T+3)); (*(T+3)) ^= (*T); (*T) ^= (*(T+3)); \
 (*(T+1)) ^= (*(T+2)); (*(T+2)) ^= (*(T+1)); (*(T+1)) ^= (*(T+2)); 
 
-void test_convert_bigend_littleend() {
     int a = 0x12345678;
     printf("0x%x\n", a);  // 0x12345678
     
@@ -10,8 +15,32 @@ void test_convert_bigend_littleend() {
     printf("0x%x\n", a);  // 0x78563412
 }
 
+/* 
+    write a macro to check a varaible is assigned or unassigned
+*/
+void test_check_assigned_unassigned() {
+    // Note, 下面要用取反的符号~，而不能用负号，否则无法判断0
+    # define IsUnsigned(X) ((X) >= 0 && (~X) >= 0)
+    
+    int a = 0;
+    unsigned int b = 0;
+    
+    if (IsUnsigned(a)) {
+        printf("a is unsigned\n"); 
+    } else {
+        printf("a is signed\n");  // hit
+    }
+    
+    if (IsUnsigned(b)) {
+        printf("b is unsigned\n");  // hit
+    } else {
+        printf("b is signed\n");
+    }
+}
+
 int main(int argc, char * argv[]) {
-    test_convert_bigend_littleend();
+    test_check_assigned_unassigned();
+    test_convert_bigendian_littleendian();
     
     return 0;
 }
