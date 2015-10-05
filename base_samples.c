dbf801a558ff8fb59ca3070a3ef2e45907505173
FinixLei <finixlei@gmail.com>
2015-10-05 16:31:44 +0800

    add test_check_type_signed_unsigned()

--- base_samples.c
+++ base_samples.c
@@ -13,12 +13,13 @@ void test_convert_bigendian_littleendian() {
     
     CONVERT((char *)&a);
     printf("0x%x\n", a);  // 0x78563412
+    printf("---------------------------------\n");
 }
 
 /* 
-    write a macro to check a varaible is assigned or unassigned
+    write a macro to check a varaible is signed or unassigned
 */
-void test_check_assigned_unassigned() {
+void test_check_signed_unassigned() {
     // Note, 下面要用取反的符号~，而不能用负号，否则无法判断0
     # define IsUnsigned(X) ((X) >= 0 && (~X) >= 0)
     
@@ -36,10 +37,33 @@ void test_check_assigned_unassigned() {
     } else {
         printf("b is signed\n");
     }
+    printf("---------------------------------\n");
+}
+
+/* 
+    write a macro to check a type is signed or unsigned
+*/
+void test_check_type_signed_unsigned() {
+    #define IsUnsignedType(TYPE) (((TYPE)0 - (TYPE)1) > (TYPE)0)
+    
+    if (IsUnsignedType(int)) {
+        printf("int is unsigned type\n");
+    } else {
+        printf("int is signed type\n");
+    }
+    
+    if (IsUnsignedType(unsigned int)) {
+        printf("unsigned int is unsigned type\n");
+    } else {
+        printf("unsigned int is signed type\n");
+    }
+    
+    printf("---------------------------------\n");
 }
 
 int main(int argc, char * argv[]) {
-    test_check_assigned_unassigned();
+    test_check_type_signed_unsigned(); 
+    test_check_signed_unassigned();
     test_convert_bigendian_littleendian();
     
     return 0;
