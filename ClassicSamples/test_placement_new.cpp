d7c5bc8c5505edf15ec93610f00af58e31d30c04
Finix Lei <leilei4@lenovo.com>
2019-03-15 00:03:39 +0800

    Update test_placement_new.cpp

--- ClassicSamples/test_placement_new.cpp
+++ ClassicSamples/test_placement_new.cpp
@@ -1,6 +1,7 @@
 /*
     所谓placement new就是在用户指定的内存位置上构建新的对象，这个构建过程不需要额外分配内存，只需要调用对象的构造函数即可。
-    placement new是怎么做的呢，说白了就是把原本new做的两步工作分开来。第一步分配内存，第二步调用类的构造函数在自己分配的内存上构建新的对象。
+    placement new是怎么做的呢?
+    说白了就是把原本new做的两步工作分开来。第一步分配内存，第二步调用类的构造函数在自己分配的内存上构建新的对象。
      
     placement new的好处：
     1）在已分配好的内存上进行对象的构建，构建速度快。
@@ -11,15 +12,22 @@
 #include <iostream>
 using namespace std;
 
-class  Foo
+class  Foo // sizeof(Foo) == 8
 {
-  char  cc;
-  float  f;
+    char  cc;
+    float  f;
  
 public :
-  void print() { std::cout <<" ADDR: " << this <<std::endl; }
-  void set_f(float _f ) { std::cout << " set f val : " <<_f <<std::endl; f = _f; }
-  void  get_f() { std::cout <<" get f val : "<<f <<std::endl; }
+    void print() { 
+        cout << " ADDR: " << this << endl; 
+    }
+    void set_f(float _f) { 
+        cout << " set f val : " << _f << endl; 
+        f = _f; 
+    }
+    void  get_f() {
+        cout <<" get f val : "<< f << endl; 
+    }
 }; 
 
  
@@ -32,15 +40,21 @@ int main() {
     memset( buff, 0 , sizeof (Foo) * N ); 
 
     // 2. 构建对象 
-    Foo *  pfoo  =   new (buff)Foo; 
+    Foo *  p1  =   new (buff)Foo; 
+    Foo *  p2  =   new (buff + sizeof(Foo))Foo; 
 
     // 3. 使用对象 
-    pfoo -> print(); 
-    pfoo -> set_f( 5.5f );
-    pfoo -> get_f(); 
+    p1 -> print(); 
+    p1 -> set_f( 5.5f );
+    p1 -> get_f(); 
+    
+    p2->print();
+    p2->set_f(10.2f);
+    p2->get_f();
 
     // 4. 析构对象，显式的调用类的析构函数。 
-    pfoo ->~ Foo(); 
+    p1->~Foo(); 
+    p2->~Foo();
 
     // 5. 销毁内存 
     delete [] buff; 
