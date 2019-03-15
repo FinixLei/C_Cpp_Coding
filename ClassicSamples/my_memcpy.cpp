f4817c56ad866395e039f37b7dd2cb9b46721cc8
Finix Lei <leilei4@lenovo.com>
2019-03-15 22:29:48 +0800

    Add my_memcpy.cpp

---
+++ ClassicSamples/my_memcpy.cpp
@@ -0,0 +1,63 @@
+#include <cstring>
+#include <iostream>
+using namespace std; 
+
+
+template <typename T> 
+void print_array(T* array, int size)
+{
+    for (int i=0; i<size; ++i) {
+        std::cout << array[i] << ' ';
+    }
+    std::cout << endl;
+}
+
+void * my_memcpy(void *dest, void *src, int size) 
+{
+    char * c_dest = (char *)dest; 
+    char * c_src = (char *)src; 
+    
+    if (c_src == c_dest) {
+        return (void *)c_dest;
+    }
+    else if (c_src < c_dest && c_dest < c_src+size) {
+        for (int count = size-1; count >= 0; --count) {
+            c_dest[count] = c_src[count];
+        }
+    }
+    else {
+        for (int i=0; i<size; ++i) {
+            c_dest[i] = c_src[i];
+        }
+        return (void *)dest;
+    }
+}
+
+
+int main()
+{
+    const int size1 = 10;
+    int *np1 = new int [size1];
+    for(int i=0; i<size1; i++) {
+        np1[i] = (i+1) * 10;
+    }
+    
+    int *np2 = new int [size1];
+    // memcpy(np2, np1, size1*sizeof(int));
+    my_memcpy(np2, np1, size1*sizeof(int));
+    print_array(np1, size1);
+    print_array(np2, size1);
+    
+    int *np3 = np1 + 2;
+    // size2 = 10 will cause crash for both memcpy and my_memcpy
+    int size2 = 5;
+    // memcpy(np3, np1, size2*sizeof(int));
+    my_memcpy(np3, np1, size2*sizeof(int));
+    print_array(np3, size2);
+    print_array(np1, size1);
+    
+    delete [] np1;
+    delete [] np2;
+    
+    return 0;
+}
