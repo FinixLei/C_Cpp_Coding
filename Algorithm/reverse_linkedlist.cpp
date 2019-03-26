77499b04043ca3f4a6739c1897e3b7628a4b32da
Finix Lei <leilei4@lenovo.com>
2019-03-26 23:01:49 +0800

    Simplify reverse_linkedlist.cpp

--- Algorithm/reverse_linkedlist.cpp
+++ Algorithm/reverse_linkedlist.cpp
@@ -21,31 +21,20 @@ Node * reverse_linkedlist_recursively(Node * head) {
 
 // way2 - in a non-recursive way
 Node * reverse_linkedlist(Node * head) {
-    if (head == NULL or head->next == NULL) {
-        return head;
-    }
+    if (head == NULL or head->next == NULL) return head;
     
     Node *p1 = head; 
-    Node *p2 = head->next;
+    Node *p2 = p1->next;
     Node *p3 = p2->next;
-    
     head->next = NULL; 
     
-    if (p3 == NULL) {
-        p2->next = p1;
-        return p2;
-    }
-    
-    while (p3->next != NULL) {
-        p2->next = p1; // 一个循环体里面只做一次反转
+    while (true) {
+        p2->next = p1; 
         p1 = p2; 
-        p2 = p3; 
-        p3 = p3->next; 
+        p2 = p3;
+        if (p3 != NULL) p3 = p3->next; 
+        else return p1;
     }
-    
-    p2->next = p1;
-    p3->next = p2;
-    return p3;
 }
 
 void print_linkedlist(Node * head) {
@@ -57,26 +46,19 @@ void print_linkedlist(Node * head) {
 }
 
 int main() {
-    Node * n1 = new Node(1); 
-    Node * n2 = new Node(2); 
-    Node * n3 = new Node(3); 
+    Node *head = new Node(1);
+    Node *h = head; 
+    for (int i=2; i<=20; ++i) {  // Or, i<=2
+        h->next = new Node(i);
+        h = h->next; 
+    }
     
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = NULL;
+    print_linkedlist(head);  // 1-20
+    head = reverse_linkedlist_recursively(head);
+    print_linkedlist(head);  // 20-1
     
-    print_linkedlist(n1);  // 1 2 3
-    
-    Node * h = reverse_linkedlist_recursively(n1);
-    print_linkedlist(h);  // 3 2 1
-    
-    h = reverse_linkedlist(h); 
-    print_linkedlist(h);  // 1 2 3
-    
-    struct Node * n4 = new Node(4); 
-    n3->next = n4;
-    h = reverse_linkedlist(h); 
-    print_linkedlist(h);  // 4 3 2 1
+    head = reverse_linkedlist(head); 
+    print_linkedlist(head);  // 1-20
     
     return 0;
 }
