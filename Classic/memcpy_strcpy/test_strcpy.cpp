7bf8a802ccc108351f766bb5f5b8984f7f42772c
Lei, Lei <lei.lei@emc.com>
2021-04-03 22:55:34 +0800

    Update test_strcpy.cpp

--- Classic/memcpy_strcpy/test_strcpy.cpp
+++ Classic/memcpy_strcpy/test_strcpy.cpp
@@ -1,19 +1,29 @@
-#include <cassert>
 #include <iostream>
 using namespace std;
 
 char * my_strcpy(char * dest, const char * source) {
-    assert(dest != NULL && source != NULL);
+    if (dest == nullptr || source == nullptr) return nullptr;
     
     int i = 0;
-    while((dest[i] = source[i++]) != '\0');
+    
+    // while((dest[i] = source[i++]) != '\0');
+    // Above may have some problem in some case, 
+    // so, personally, I like to use more readable style as below. 
+    while (source[i] != 0) {
+        dest[i] = source[i];
+        ++i;
+    }
+    
     return dest;
 }
 
 int main() {
-    char dest[100];
+    char *dest = new char [100]; 
     char source[] = "Hello, my_strcpy()!"; 
     
     my_strcpy(dest, source);
     cout << dest << endl;
+    
+    delete [] dest;
+    return 0;
 }
